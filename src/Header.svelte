09936c26bd4a00d6c9b67007a6f03d77ea808d77
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-07-06 05:05:30 -0300

    Update Header.svelte

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <header>
-<h1>🔫💬</h1>
+<h1>💱</h1>
   {#if $username}
     <div class="user-bio">
       
@@ -20,6 +20,6 @@
 
     {:else}
 
-      <h3>Gun.js Chat</h3>
+      <h3>X-Chain Decentralized Club</h3>
   {/if}
 </header>
